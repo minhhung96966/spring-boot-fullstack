445c5fca5ac239efcd4066eb6df5a54817546d2c
David Nguyen <minhhung96966@gmail.com>
2023-06-19 11:54:08 +0700

    update Dockerrun and cd

--- backend/src/main/java/com/minhhung/PingPongController.java
+++ backend/src/main/java/com/minhhung/PingPongController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
         return new PingPong("Pong");
     }
+
 }
